d02112a339f057e293a636019e9850feff0fc6d8
Cassidy King <cassidyking17@gmail.com>
2021-01-04 13:32:01 -0500

    made adjustments to uploads and added a reset button

--- src/components/formatting/inputRow.tsx
+++ src/components/formatting/inputRow.tsx
@@ -5,17 +5,21 @@ import FileUpload from '../fileUpload/FileUpload';
 interface Props {
   onMeasureUpload: (files: any) => void;
   onPatientUpload: (files: any) => void;
+  measureFileName: string | null;
+  patientFileName: string | null;
 }
 export default function InputRow(props: Props) {
   return (
     <React.Fragment>
       <Grid item xs={6}>
-        <h2>Measure Bundle:</h2>
-        {<FileUpload onDrop={props.onMeasureUpload} />}
+        <h2>Measure Bundle: </h2>
+        <h3>{props.measureFileName}</h3>
+        {props.measureFileName === null && <FileUpload onDrop={props.onMeasureUpload} />}
       </Grid>
       <Grid item xs={6}>
-        <h2>Patient Bundle:</h2>
-        {<FileUpload onDrop={props.onPatientUpload} />}
+        <h2>Patient Bundle: </h2>
+        <h3>{props.patientFileName}</h3>
+        {props.patientFileName === null && <FileUpload onDrop={props.onPatientUpload} />}
       </Grid>
     </React.Fragment>
   );
