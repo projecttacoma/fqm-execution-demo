ad8a751d247626743a2916d3344a6c1cefdcb0b8
Elsa Perelli <EPERELLI@MITRE.ORG>
2021-07-20 10:23:14 -0400

    Added guidance response information to each guidance response rendered for a detected issue

--- src/components/Results/DetectedIssueResources.tsx
+++ src/components/Results/DetectedIssueResources.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Accordion, AccordionSummary, AccordionDetails, Grid } from '@material-ui/core';
+import { Accordion, AccordionSummary, AccordionDetails, Grid, Typography } from '@material-ui/core';
 import { R4 } from '@ahryman40k/ts-fhir-types';
 import fhirpath from 'fhirpath';
 
@@ -19,18 +19,69 @@ const DetectedIssueResources: React.FC<Props> = ({ detectedIssue }) => {
             <AccordionSummary>
               <Grid item xs>
                 <h4>
-                  {' '}
                   {fhirpath.evaluate(response, 'resourceType')} {guidanceResponseId}{' '}
                 </h4>
               </Grid>
             </AccordionSummary>
             <AccordionDetails>
-              <Grid item xs>
-                <Grid item xs>
-                  code: {fhirpath.evaluate(response, 'reasonCode.coding.code')}
+              <Grid container item xs direction="row">
+                <Grid container xs={6} direction="column">
+                  <Grid item xs>
+                    <h4>dataRequirement:</h4>
+                  </Grid>
+                  <Grid item xs>
+                    type: {fhirpath.evaluate(response, 'dataRequirement.type')}
+                  </Grid>
+                  <Grid item xs>
+                    <h5>codeFilter:</h5>
+                  </Grid>
+                  <Grid>path: {fhirpath.evaluate(response, 'dataRequirement.codeFilter.path[0]')}</Grid>
+                  <Grid item xs>
+                    <Typography style={{ overflowWrap: 'break-word' }}>
+                      valueSet: {fhirpath.evaluate(response, 'dataRequirement.codeFilter.valueSet')}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs>
+                    path: {fhirpath.evaluate(response, 'dataRequirement.codeFilter.path[1]')}
+                  </Grid>
+                  <Grid item xs>
+                    {fhirpath.evaluate(response, 'dataRequirement.codeFilter.code.code').map((code: string) => {
+                      return (
+                        <Grid item xs>
+                          code: {code}
+                        </Grid>
+                      );
+                    })}
+                  </Grid>
+                  <Grid item xs>
+                    <h5>dateFilter:</h5>
+                  </Grid>
+                  <Grid item xs>
+                    path: {fhirpath.evaluate(response, 'dataRequirement.dateFilter.path')}
+                  </Grid>
+                  <Grid item xs>
+                    valuePeriod:
+                  </Grid>
+                  <Grid item xs>
+                    start: {fhirpath.evaluate(response, 'dataRequirement.dateFilter.valuePeriod.start')}
+                  </Grid>
+                  <Grid item xs>
+                    end: {fhirpath.evaluate(response, 'dataRequirement.dateFilter.valuePeriod.end')}
+                  </Grid>
                 </Grid>
-                <Grid item xs>
-                  display: {fhirpath.evaluate(response, 'reasonCode.coding.display')}
+                <Grid container item xs={6} direction="column">
+                  <Grid item xs>
+                    <h4>reasonCode:</h4>
+                  </Grid>
+                  <Grid item xs>
+                    system: {fhirpath.evaluate(response, 'reasonCode.coding.system')}
+                  </Grid>
+                  <Grid item xs>
+                    code: {fhirpath.evaluate(response, 'reasonCode.coding.code')}
+                  </Grid>
+                  <Grid item xs>
+                    display: {fhirpath.evaluate(response, 'reasonCode.coding.display')}
+                  </Grid>
                 </Grid>
               </Grid>
             </AccordionDetails>
