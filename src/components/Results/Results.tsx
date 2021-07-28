d32e746f420bea2756d96ee2a4957fe3e83d099b
sarahmcdougall <smcdougall@mitre.org>
2021-07-28 12:35:24 -0400

    Added tabs for output views

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,4 +1,6 @@
-import { makeStyles, Grid, IconButton } from '@material-ui/core';
+import { makeStyles, Grid, IconButton, AppBar, Tab, Tabs } from '@material-ui/core';
+import TabPanel from '@material-ui/lab/TabPanel';
+import TabContext from '@material-ui/lab/TabContext';
 import ReactJson from 'react-json-view';
 import parse from 'html-react-parser';
 import fileDownload from 'js-file-download';
@@ -11,6 +13,7 @@ import { useRecoilValue } from 'recoil';
 import { calculationOptionsState, outputTypeState, resultsState } from '../../state';
 import { R4 } from '@ahryman40k/ts-fhir-types';
 import fhirpath from 'fhirpath';
+
 
 const useStyles = makeStyles(() => ({
   highlightedMarkup: {
@@ -33,43 +36,29 @@ const Results: React.FC<Props> = ({ measureFile, patientFile, htmls }) => {
   const results = useRecoilValue(resultsState);
   const detectedIssues = fhirpath.evaluate(results, 'Bundle.entry.resource.DetectedIssue');
 
+  const style = {
+     minWidth: '25%'
+  }
+
+  const [value, setValue] = React.useState('0');
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+    setValue(newValue);
+  }
+
   return (
     <Grid container xs={12} direction="column" spacing={2}>
       <h2>Results:</h2>
-      <Grid container item xs={12} direction="row" justify="center" alignItems="center">
-        {outputType === 'measureReports' &&
-          calculationOptions.reportType === 'individual' &&
-          fhirpath.evaluate(results, 'MeasureReport.group').map((group: R4.IMeasureReport_Group) => {
-            const id = results ? fhirpath.evaluate(results, 'MeasureReport.subject.reference')[0].split('/') : '';
-            return (
-              <Grid container item xs={12} direction="column" justify="center" alignItems="center" key={group.id}>
-                <h2>{group.id} Population Results</h2>
-                <PopulationResults key={group.id} results={group} id={id} />
-              </Grid>
-            );
-          })}
-        {results &&
-          detectedIssues.map((issue: R4.IDetectedIssue, index: number) => {
-            const detectedIssueId = fhirpath.evaluate(issue, 'id');
-            return (
-              <Grid
-                container
-                item
-                xs={12}
-                direction="column"
-                justify="center"
-                alignItems="center"
-                key={detectedIssueId}
-              >
-                <h3>Detected Issue {index + 1}</h3>
-                <h4>{fhirpath.evaluate(issue, 'contained.GuidanceResponse').length} Guidance Response(s)</h4>
-                <DetectedIssueResources detectedIssue={issue} />
-              </Grid>
-            );
-          })}
-      </Grid>
-      <Grid container item xs={12} direction="row">
-        <Grid item xs>
+      <TabContext value={value}>
+        <AppBar position="static">
+          <Tabs aria-label="results tabs" onChange = {handleChange} value = {value} indicatorColor="primary" textColor="primary" centered>
+            <Tab style={style} label="Raw JSON" value='0'/>
+            <Tab style={style} label="Highlighted HTML" value='1' />
+            <Tab style={style} label="Tabular" value='2' />
+            <Tab style={style} label="Accordion" value='3' />
+          </Tabs>
+        </AppBar>
+        <TabPanel value = '0'>
           <Grid container item xs direction="row">
             {results && <h2>JSON:</h2>}
             {results && (
@@ -92,21 +81,60 @@ const Results: React.FC<Props> = ({ measureFile, patientFile, htmls }) => {
           {patientFile && (
             <ReactJson src={patientFile} enableClipboard={true} theme="shapeshifter:inverted" collapsed={2} />
           )}
-        </Grid>
-        <Grid item xs>
-          {results &&
-            htmls &&
-            htmls.map(html => {
-              return (
-                <div key={html.groupId} className={classes.highlightedMarkup}>
-                  <h2>HTML:</h2>
-                  {parse(html.html)}
-                </div>
-              );
+        </TabPanel>
+        <TabPanel value = '1'> 
+          <Grid item xs>
+              {results &&
+                htmls &&
+                htmls.map(html => {
+                  return (
+                    <div key={html.groupId} className={classes.highlightedMarkup}>
+                      <h2>HTML:</h2>
+                      {parse(html.html)}
+                    </div>
+                  );
+                })}
+            </Grid>
+        </TabPanel>
+        <TabPanel value = '2'>
+          <Grid container item xs={12} direction="row" justify="center" alignItems="center">
+            {outputType === 'measureReports' &&
+              calculationOptions.reportType === 'individual' &&
+              fhirpath.evaluate(results, 'MeasureReport.group').map((group: R4.IMeasureReport_Group) => {
+                const id = results ? fhirpath.evaluate(results, 'MeasureReport.subject.reference')[0].split('/') : '';
+                return (
+                  <Grid container item xs={12} direction="column" justify="center" alignItems="center" key={group.id}>
+                    <h2>{group.id} Population Results</h2>
+                    <PopulationResults key={group.id} results={group} id={id} />
+                  </Grid>
+                );
             })}
-        </Grid>
-      </Grid>
+          </Grid>
+        </TabPanel>
+        <TabPanel value = '3'>
+          {results &&
+              detectedIssues.map((issue: R4.IDetectedIssue, index: number) => {
+                const detectedIssueId = fhirpath.evaluate(issue, 'id');
+                return (
+                  <Grid
+                    container
+                    item
+                    xs={12}
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                    key={detectedIssueId}
+                  >
+                    <h3>Detected Issue {index + 1}</h3>
+                    <h4>{fhirpath.evaluate(issue, 'contained.GuidanceResponse').length} Guidance Response(s)</h4>
+                    <DetectedIssueResources detectedIssue={issue} />
+                  </Grid>
+                );
+              })}
+        </TabPanel>
+      </TabContext>
     </Grid>
+    
   );
 };
 
