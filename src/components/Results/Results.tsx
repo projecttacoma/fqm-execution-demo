d5c827b605314b2c595da57a4636e130a9d655e3
sarahmcdougall <smcdougall@mitre.org>
2021-07-28 16:35:55 -0400

    Adjusted for dynamic tab generation

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -46,44 +46,38 @@ const Results: React.FC<Props> = ({ measureFile, patientFile, htmls }) => {
     setValue(newValue);
   }
 
-  return (
-    <Grid container xs={12} direction="column" spacing={2}>
-      <h2>Results:</h2>
-      <TabContext value={value}>
-        <AppBar position="static">
-          <Tabs aria-label="results tabs" onChange = {handleChange} value = {value} indicatorColor="primary" textColor="primary" centered>
-            <Tab style={style} label="Raw JSON" value='0'/>
-            <Tab style={style} label="Highlighted HTML" value='1' />
-            <Tab style={style} label="Tabular" value='2' />
-            <Tab style={style} label="Accordion" value='3' />
-          </Tabs>
-        </AppBar>
-        <TabPanel value = '0'>
-          <Grid container item xs direction="row">
-            {results && <h2>JSON:</h2>}
-            {results && (
-              <IconButton
-                onClick={() => {
-                  fileDownload(
-                    JSON.stringify(results, null, 2),
-                    measureFile.name?.includes('.json')
-                      ? `results-${measureFile.name}`
-                      : `results-${measureFile.name}.json`
-                  );
-                }}
-              >
-                <GetApp fontSize="small" />
-              </IconButton>
-            )}
-          </Grid>
-          {results && <ReactJson src={results} enableClipboard={true} theme="shapeshifter:inverted" collapsed={2} />}
-          {patientFile && <h2>Patient Bundle:</h2>}
-          {patientFile && (
-            <ReactJson src={patientFile} enableClipboard={true} theme="shapeshifter:inverted" collapsed={2} />
-          )}
-        </TabPanel>
-        <TabPanel value = '1'> 
-          <Grid item xs>
+  const displayJSONResults = () => {
+    return (
+    <>
+    <Grid container item xs direction="row">
+      {results && <h2>JSON:</h2>}
+      {results && (
+        <IconButton
+          onClick={() => {
+            fileDownload(
+              JSON.stringify(results, null, 2),
+              measureFile.name?.includes('.json')
+                ? `results-${measureFile.name}`
+                : `results-${measureFile.name}.json`
+            );
+          }}
+        >
+          <GetApp fontSize="small" />
+        </IconButton>
+      )}
+    </Grid>
+    {results && <ReactJson src={results} enableClipboard={true} theme="shapeshifter:inverted" collapsed={2} />}
+    {patientFile && <h2>Patient Bundle:</h2>}
+    {patientFile && (
+      <ReactJson src={patientFile} enableClipboard={true} theme="shapeshifter:inverted" collapsed={2} />
+    )}
+    </>
+    )
+  }
+
+  const displayHTMLResults = () => {
+    return (
+      <Grid item xs>
               {results &&
                 htmls &&
                 htmls.map(html => {
@@ -95,44 +89,108 @@ const Results: React.FC<Props> = ({ measureFile, patientFile, htmls }) => {
                   );
                 })}
             </Grid>
-        </TabPanel>
-        <TabPanel value = '2'>
-          <Grid container item xs={12} direction="row" justify="center" alignItems="center">
-            {outputType === 'measureReports' &&
-              calculationOptions.reportType === 'individual' &&
-              fhirpath.evaluate(results, 'MeasureReport.group').map((group: R4.IMeasureReport_Group) => {
-                const id = results ? fhirpath.evaluate(results, 'MeasureReport.subject.reference')[0].split('/') : '';
-                return (
-                  <Grid container item xs={12} direction="column" justify="center" alignItems="center" key={group.id}>
-                    <h2>{group.id} Population Results</h2>
-                    <PopulationResults key={group.id} results={group} id={id} />
-                  </Grid>
-                );
-            })}
-          </Grid>
-        </TabPanel>
-        <TabPanel value = '3'>
-          {results &&
-              detectedIssues.map((issue: R4.IDetectedIssue, index: number) => {
-                const detectedIssueId = fhirpath.evaluate(issue, 'id');
-                return (
-                  <Grid
-                    container
-                    item
-                    xs={12}
-                    direction="column"
-                    justify="center"
-                    alignItems="center"
-                    key={detectedIssueId}
-                  >
-                    <h3>Detected Issue {index + 1}</h3>
-                    <h4>{fhirpath.evaluate(issue, 'contained.GuidanceResponse').length} Guidance Response(s)</h4>
-                    <DetectedIssueResources detectedIssue={issue} />
-                  </Grid>
-                );
-              })}
-        </TabPanel>
-      </TabContext>
+    )
+  }
+
+  const displayTabularResults = () => {
+    return (
+      <Grid container item xs={12} direction="row" justify="center" alignItems="center">
+          {
+            fhirpath.evaluate(results, 'MeasureReport.group').map((group: R4.IMeasureReport_Group) => {
+              const id = results ? fhirpath.evaluate(results, 'MeasureReport.subject.reference')[0].split('/') : '';
+              return (
+                <Grid container item xs={12} direction="column" justify="center" alignItems="center" key={group.id}>
+                  <h2>{group.id} Population Results</h2>
+                  <PopulationResults key={group.id} results={group} id={id} />
+                </Grid>
+              );
+          })}
+        </Grid>
+    )
+  }
+
+  const displayAccordionResults = () => {
+    return (
+      <>
+      {results &&
+        detectedIssues.map((issue: R4.IDetectedIssue, index: number) => {
+          const detectedIssueId = fhirpath.evaluate(issue, 'id');
+          return (
+            <Grid
+              container
+              item
+              xs={12}
+              direction="column"
+              justify="center"
+              alignItems="center"
+              key={detectedIssueId}
+            >
+              <h3>Detected Issue {index + 1}</h3>
+              <h4>{fhirpath.evaluate(issue, 'contained.GuidanceResponse').length} Guidance Response(s)</h4>
+              <DetectedIssueResources detectedIssue={issue} />
+            </Grid>
+          );
+        })}
+      </>
+    )
+  }
+
+  const shouldDisplayHTML = () => {
+    return htmls.length > 0;
+  }
+
+  const shouldDisplayAccordion = () => {
+    return outputType === "gapsInCare";
+  }
+
+  const shouldDisplayTabular = () => {    
+    return outputType === "measureReports" && calculationOptions.reportType === 'individual';
+  }
+
+  const shouldDisplayTabs = () => {
+    return (shouldDisplayHTML() || shouldDisplayAccordion() || shouldDisplayTabular());
+  }
+  
+  return (
+    <Grid container xs={12} direction="column" spacing={2}>
+      <h2>Results:</h2> {
+        shouldDisplayTabs() ? 
+        <TabContext value={value}> 
+          <AppBar position="static">
+            <Tabs TabIndicatorProps={{style: {background:'white'}}} aria-label="results tabs" onChange = {handleChange} value = {value} indicatorColor="primary" centered>
+              <Tab style={style} label="Raw JSON" value='0' />
+              {
+                shouldDisplayHTML() && <Tab style={style} label="Highlighted HTML" value='1' />
+              } 
+              {
+                shouldDisplayTabular() && <Tab style={style} label="Tabular" value='2' />
+              }
+              {
+                shouldDisplayAccordion() && <Tab style={style} label="Accordion" value='3' />
+              }
+            </Tabs>
+          </AppBar>
+
+          <TabPanel value = '0'>
+            {displayJSONResults()}
+          </TabPanel>
+          {shouldDisplayHTML() && 
+            <TabPanel value = '1'> 
+              {displayHTMLResults()}
+            </TabPanel>
+          }
+          {shouldDisplayTabular() && 
+            <TabPanel value = '2'>
+              {displayTabularResults()}
+            </TabPanel>
+          }
+          {shouldDisplayAccordion() && 
+            <TabPanel value = '3'>
+              {displayAccordionResults()}
+            </TabPanel>}
+          </TabContext>
+        : displayJSONResults()
+      } 
     </Grid>
     
   );
